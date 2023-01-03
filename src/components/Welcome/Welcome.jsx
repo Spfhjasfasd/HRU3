3433725c660cc26120e8e698604daefbfdf3d268
Spfhjasfasd <ajk215@case.edu>
2023-01-03 01:26:02 -0500

    Imported from Quest

--- src/components/Welcome/Welcome.jsx
+++ src/components/Welcome/Welcome.jsx
@@ -15,64 +15,62 @@
 import React from 'react';
 import { styled } from '@mui/material/styles';
 import Navbar2 from 'src/components/Navbar2/Navbar2';
-import useWelcome from 'src/components/Welcome/useWelcome';
 
 const ScreenDesktop = styled('div')({
   backgroundColor: `rgba(191, 38, 158, 1)`,
   display: `flex`,
   position: `relative`,
   isolation: `isolate`,
-  flexDirection: `column`,
-  height: `782.5px`,
+  flexDirection: `row`,
+  width: '100%',
+  height: `887px`,
   justifyContent: `flex-start`,
-  alignItems: `center`,
+  alignItems: `flex-start`,
   padding: `0px`,
   boxSizing: `border-box`,
   overflow: `hidden`,
-  width: '100%',
 });
 
-const Navbar21 = styled(Navbar2, {
-  shouldForwardProp: (prop) => !['data'].includes(prop.toString()),
-})(({ theme, data }) => ({
-  height: data.size === 'mobile' ? 'unset' : `60px`,
-  width: data.size === 'mobile' ? `396px` : `1440px`,
-  margin: `0px`,
+const Navbar21 = styled(Navbar2)(({ theme }) => ({
+  width: `1440px`,
+  height: `60px`,
+  position: `absolute`,
+  left: `0px`,
+  top: `0px`,
 }));
 
-const Hrucoin = styled('div', {
-  shouldForwardProp: (prop) => !['data'].includes(prop.toString()),
-})(({ data }) => ({
+const Q1 = styled('div')({
   textAlign: `left`,
   whiteSpace: `pre-wrap`,
   color: `rgba(0, 0, 0, 1)`,
   fontStyle: `normal`,
   fontFamily: `Nova Round`,
   fontWeight: `400`,
-  fontSize: data.size === 'mobile' ? `40px` : `96px`,
-  letterSpacing: data.size === 'mobile' ? `-2px` : `-4.8px`,
+  fontSize: `32px`,
+  letterSpacing: `-1.6px`,
   textDecoration: `none`,
   lineHeight: `62px`,
   textTransform: `none`,
-  margin: data.size === 'mobile' ? `70px 0px 0px 0px` : `90px 0px 0px 0px`,
-}));
+  position: `absolute`,
+  left: `462px`,
+  top: `140px`,
+});
 
-const Button1 = styled('div', {
-  shouldForwardProp: (prop) => !['data'].includes(prop.toString()),
-})(({ data }) => ({
+const Button1 = styled('div')({
   backgroundColor: `rgba(123, 97, 255, 1)`,
   boxShadow: `0px 6px 7px rgba(123, 97, 255, 0.3)`,
   borderRadius: `8px`,
   display: `flex`,
-  position: `relative`,
+  position: `absolute`,
   isolation: `isolate`,
   flexDirection: `row`,
   justifyContent: `center`,
   alignItems: `flex-start`,
   padding: `14px 50px`,
   boxSizing: `border-box`,
-  margin: data.size === 'mobile' ? `70px 0px 0px 0px` : `90px 0px 0px 0px`,
-}));
+  left: `622px`,
+  top: `282px`,
+});
 
 const LearnMore = styled('div')({
   textAlign: `left`,
@@ -88,35 +86,32 @@ const LearnMore = styled('div')({
   margin: `0px`,
 });
 
-const Q1 = styled('div', {
-  shouldForwardProp: (prop) => !['data'].includes(prop.toString()),
-})(({ data }) => ({
+const Hrucoin = styled('div')({
   textAlign: `left`,
   whiteSpace: `pre-wrap`,
   color: `rgba(0, 0, 0, 1)`,
   fontStyle: `normal`,
   fontFamily: `Nova Round`,
   fontWeight: `400`,
-  fontSize: data.size === 'mobile' ? `16px` : `32px`,
-  letterSpacing: data.size === 'mobile' ? `-0.8px` : `-1.6px`,
+  fontSize: `96px`,
+  letterSpacing: `-4.8px`,
   textDecoration: `none`,
   lineHeight: `62px`,
   textTransform: `none`,
-  margin: data.size === 'mobile' ? `70px 0px 0px 0px` : `90px 0px 0px 0px`,
-  width: data.size === 'mobile' ? `289px` : 'unset',
-}));
+  position: `absolute`,
+  left: `467px`,
+  top: `449px`,
+});
 
 function Welcome(props) {
-  const { data } = useWelcome();
-
   return (
     <ScreenDesktop>
-      <Navbar21 data={data} />
-      <Hrucoin data={data}>{`HRUCOIN`}</Hrucoin>
-      <Button1 data={data}>
+      <Navbar21 />
+      <Q1>{`Innovating Blockchain Health Services`}</Q1>
+      <Button1>
         <LearnMore>{`Learn More`}</LearnMore>
       </Button1>
-      <Q1 data={data}>{`Innovating Blockchain Health Services`}</Q1>
+      <Hrucoin>{`HRUCOIN`}</Hrucoin>
     </ScreenDesktop>
   );
 }
